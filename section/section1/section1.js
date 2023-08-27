91c0d8b91894051ee4ae7cd1a2ae9bbd06b4130d
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-08-27 14:16:58 +0900

    Update section1.js

--- section/section1/section1.js
+++ section/section1/section1.js
@@ -1,21 +1,16 @@
-q1 = "ここに回答を入力";
+q1 = "//";
 
-q2 = "ここに回答を入力";
+q2 = "ver";
 
-q3 = `
+q3 = `let country;
+          country= "Japan";
+          country= "America"`;
 
-ここに回答（コード）を入力
+q4 = `let Japan = {capital:"東京"};
+      Japan["lang"]= "日本語"`;
 
-`;
+q5 = [1,2,5,6];
 
-q4 = `
-
-ここに回答（コード）を入力
-
-`;
-
-q5 = [];
-
-q6 = "ここに回答を入力";
+q6 = "キャメルケース";
 
 module.exports = { q1, q2, q3, q4, q5, q6 };
