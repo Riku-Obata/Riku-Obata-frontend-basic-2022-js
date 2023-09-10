b7e1f0e83396787d3f73dc6cbe8dc19ad945b8ea
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-09-10 14:34:22 +0900

    Update section5.js

--- section/section5/section5.js
+++ section/section5/section5.js
@@ -1,15 +1,19 @@
 function score(num){
-  if (num >= 80 && num <=100){
-    return "合格";
-   } else if (num >= 60 && num <80){
-    return "追試";
-   } else if (num >= 0 && num <60){
-    return "不合格";
-   } else if (num === undefind){
-    return "後日受験";
-   }
-  else (isNaN(num) || num < 0 || num > 100) {
-    return "0以上100未満の数字を入れてください"};
+ if (num === undefined){
+   return "後日受験";
+ } else if (isNaN(num) || num < 0 || num > 100) {
+   return "0以上100未満の数字を入れてください";
+ } else if ( num >=80 && num <=100) {
+   return "合格"
+ } else if ( num >=60 && num <80) {
+   return "追試"
+ } else (num >=0 && num <60) {
+   return "不合格"
  }
+}
+  
+  
+   
+  
 
 module.exports = { score };
