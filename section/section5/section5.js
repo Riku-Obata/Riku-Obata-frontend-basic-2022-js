e0278be9eff9a51daac2cf199313ab00801a7cae
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-09-10 13:59:44 +0900

    Update section5.js

--- section/section5/section5.js
+++ section/section5/section5.js
@@ -1,3 +1,15 @@
 const score = null;
+  if (score >= 80 && score <=100){
+    console.log("合格")
+   } else if (score >= 60 && score <80){
+    console.log("追試")
+   } else if (score >= 0 && score <60){
+    console.log("不合格")
+   } else if (score = null){
+    console.log("後日受験")
+   }
+  else {
+    console.log("0以上100未満の数字を入れてください")
+
 
 module.exports = { score };
