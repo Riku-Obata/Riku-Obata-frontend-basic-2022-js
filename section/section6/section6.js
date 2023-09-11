16c500b8fd0defa71d5f7b9c4325226973d09026
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-09-11 20:16:25 +0900

    Update section6.js

--- section/section6/section6.js
+++ section/section6/section6.js
@@ -1,3 +1,11 @@
-const sum = null;
+function sum(num1,num2) {
+  let sum =0;
+  const start = Math.min(num1, num2);
+  const end = Math.max(num1, num2);
+  for (let i=start ; i <= end ; i++) {
+    sum +=i;
+  }
+  return sum;
+}
 
 module.exports = { sum };
