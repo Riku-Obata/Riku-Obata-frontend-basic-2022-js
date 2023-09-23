8e7c899c2d868fea6ad8256549ecdbb1775bc75d
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-09-23 19:53:42 +0900

    Update section9.js

--- section/section9/section9.js
+++ section/section9/section9.js
@@ -1,34 +1,38 @@
-const userIdList = (jsonDatas) =>{
+const userIdList = (jsonDatas) => {
   const returnUserId = [];
 
-  const allUserIds = jsonDates.map((jsonData) => {
+  const allUserIds = jsonDatas.map((jsonData) => {
     return jsonData.userId;
-  }
-  for (userIdList or allUserIds) {
-    const getUserId = returnUserId.findindex((eachReturnUserId) =>{
+  });
+
+  for (userId of allUserIds) {
+    const getUserId = returnUserId.findIndex((eachReturnUserId) => {
       return eachReturnUserId === userId;
-       });
-    if(userId === -1) {
+    });
+
+    if (getUserId === -1) {
       returnUserId.push(userId);
-     }
     }
-   return returnUserId;
+  }
+
+  return returnUserId;
 };
 
-const fixData = (jsonDatas) =>{
+const fixData = (jsonDatas) => {
   const userIds = userIdList(jsonDatas);
   const array = [];
 
-  for (userId or userIds) {
-    const macthedUserdata = jsondata.filter((jsonData) =>jsonDatas.userId === userId
-  );
+  for (userId of userIds) {
+    const matchedUserData = jsonDatas.filter(
+      (jsonData) => jsonData.userId === userId
+    );
     for (eachMatchedUserData of matchedUserData) {
       delete eachMatchedUserData.userId;
     }
     array.push(matchedUserData);
   }
 
-  const newDatas = userIds.map((id,index) => {
+  const newDatas = userIds.map((id, index) => {
     return {
       userId: id,
       datas: array[index],
