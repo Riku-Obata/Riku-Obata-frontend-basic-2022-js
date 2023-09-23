68a76313157d54d689dc5c64fe567d977b4b31d6
Riku <riku.o-222-my.friend@ezweb.ne.jp>
2023-09-23 16:07:22 +0900

    Update section9.js

--- section/section9/section9.js
+++ section/section9/section9.js
@@ -1,5 +1,40 @@
-const userIdList = null;
+const userIdList = (jsonDatas) =>{
+  const returnUserId = [];
 
-const fixData = null;
+  const allUserIds = jsonDates.map((jsonData) => {
+    return jsonData.userId;
+  }
+  for (userIdList or allUserIds) {
+    const getUserId = returnUserId.findindex((eachReturnUserId) =>{
+      return eachReturnUserId === userId;
+       });
+    if(userId === -1) {
+      returnUserId.push(userId);
+     }
+    }
+   return returnUserId;
+};
+
+const fixData = (jsonDatas) =>{
+  const userIds = userIdList(jsonDatas);
+  const array = [];
+
+  for (userId or userIds) {
+    const macthedUserdata = jsondata.filter((jsonData) =>jsonDatas.userId === userId
+  );
+    for (eachMatchedUserData of matchedUserData) {
+      delete eachMatchedUserData.userId;
+    }
+    array.push(matchedUserData);
+  }
+
+  const newDatas = userIds.map((id,index) => {
+    return {
+      userId: id,
+      datas: array[index],
+    };
+  });
+  return newDatas;
+};
 
 module.exports = { userIdList, fixData };
